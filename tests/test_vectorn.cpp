82dbee8e3b48ce2ce97661c13e70e0458eb188fd
Sietze van Buuren <s.van.buuren@gmail.com>
2024-12-21 16:11:11 +0100

    test: VectorN test for submspan and types
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- tests/test_vectorn.cpp
+++ tests/test_vectorn.cpp
@@ -1,13 +1,18 @@
 #include <vector>
 #include <gtest/gtest.h>
 #include <utility>
+#include "experimental/__p0009_bits/layout_stride.hpp"
 #include "vectorn.hpp"
+
+using Vector = std::vector<double>;
+using Vector2 = std::vector<std::vector<double>>;
+using Vector3 = std::vector<std::vector<std::vector<double>>>;
+
+template<class T>unsigned char identify(T&& v) {return v;}
+
 
 TEST(TestVectorN, test_vectorn)
 {
-    using Vector = std::vector<double>;
-    using Vector2 = std::vector<std::vector<double>>;
-    using Vector3 = std::vector<std::vector<std::vector<double>>>;
     Vector3 nvec1 = {
                     {
                         {1, 2, 3},
@@ -37,12 +42,52 @@ TEST(TestVectorN, test_vectorn)
     ASSERT_EQ(vec3(4), 5);
 }
 
+using Pair = std::pair<std::size_t, std::size_t>;
+using Mdspan = std::mdspan<const double, std::dextents<std::size_t, 2>, std::layout_stride>;
+
+TEST(TestSubmdspan, test_submdspan)
+{
+    Vector vec = {1, 2, 2, 2, 3, 3, 3, 3, 4};
+
+    //Mdspan33 view = std::mdspan<const double, std::extents<std::size_t, 3, 3>>(vec.data(), 3, 3);
+    Mdspan view = std::mdspan(vec.data(), 3, 3);
+    ASSERT_EQ(view(0, 0), 1);
+    ASSERT_EQ(view(1, 1), 3);
+
+    Mdspan subview = std::submdspan(view, Pair{0, 2}, Pair{0, 2});
+    ASSERT_EQ(subview(0, 0), 1);
+    ASSERT_EQ(subview(0, 1), 2);
+    ASSERT_EQ(subview(0, 2), 2);
+    ASSERT_EQ(subview(1, 0), 2);
+    ASSERT_EQ(subview(1, 1), 3);
+    ASSERT_EQ(subview(1, 2), 3);
+    ASSERT_EQ(subview(2, 0), 3);
+    ASSERT_EQ(subview(2, 1), 3);
+    ASSERT_EQ(subview(2, 2), 4);
+}
+
+
 TEST(TestVectorNMdspan, test_vectorn_mdspan)
 {
-    using Vector3 = std::vector<std::vector<std::vector<double>>>;
-    using Pair = std::pair<std::size_t, std::size_t>;
+    Vector2 vec2 = {
+                   {1, 2, 2},
+                   {2, 3, 3},
+                   {3, 3, 4}
+                   };
 
-    Vector3 vec1 = {
+    vec::VectorN<double, 2> nvec2(vec2);
+
+    auto view2 = nvec2.mdspan();
+    ASSERT_EQ(view2(0, 0), 1);
+    ASSERT_EQ(view2(1, 1), 3);
+
+    auto subview2 = nvec2.submdspan(Pair{0, 1}, Pair{0, 1});
+    ASSERT_EQ(subview2(0, 0), 1);
+    ASSERT_EQ(subview2(0, 1), 2);
+    ASSERT_EQ(subview2(1, 0), 2);
+    ASSERT_EQ(subview2(1, 1), 3);
+
+    Vector3 vec3 = {
                     {
                         {1, 2, 3},
                         {4, 5, 6}
@@ -52,15 +97,15 @@ TEST(TestVectorNMdspan, test_vectorn_mdspan)
                         {10, 11, 12}
                         }
                     };
-    vec::VectorN<double, 3> nvec1(vec1);
+    vec::VectorN<double, 3> nvec3(vec3);
 
-    auto view = nvec1.mdspan();
-    ASSERT_EQ(view(0, 0, 0), 1);
-    ASSERT_EQ(view(1, 1, 2), 12);
+    auto view3 = nvec3.mdspan();
+    ASSERT_EQ(view3(0, 0, 0), 1);
+    ASSERT_EQ(view3(1, 1, 2), 12);
 
-    auto view2 = nvec1.submdspan(Pair{1, 2}, Pair{0, 1}, Pair{1, 2});
-    ASSERT_EQ(view2(0, 0, 0), 8);
-    ASSERT_EQ(view2(0, 0, 1), 9);
-    ASSERT_EQ(view2(0, 1, 0), 11);
-    ASSERT_EQ(view2(0, 1, 1), 12);
+    auto subview3 = nvec3.submdspan(Pair{1, 2}, Pair{0, 1}, Pair{1, 2});
+    ASSERT_EQ(subview3(0, 0, 0), 8);
+    ASSERT_EQ(subview3(0, 0, 1), 9);
+    ASSERT_EQ(subview3(0, 1, 0), 11);
+    ASSERT_EQ(subview3(0, 1, 1), 12);
 }
