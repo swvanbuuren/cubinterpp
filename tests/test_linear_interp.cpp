900d07f1581dfd95d1a322caa1d8de2991095e71
Sietze van Buuren <s.van.buuren@gmail.com>
2024-12-30 18:04:57 +0100

    test: add scripting to generate test data with corresponding tests
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- tests/test_linear_interp.cpp
+++ tests/test_linear_interp.cpp
@@ -44,18 +44,79 @@ TEST(TestLinearCell2D, test_linear_cell_2d) {
 }
 
 
-TEST(TestInterp2D, test_linear_interp_2d) {
-    Vector x = {0.0, 1.0, 2.0};
-    Vector y = {0.0, 1.0, 2.0};
-    Vector2 f = {{1.0, 2.0, 2.0},
-                 {2.0, 3.0, 3.0},
-                 {3.0, 3.0, 4.0}};
-
+testing::AssertionResult Interp2DAssertions(Vector x, Vector y, Vector2 f, Vector x_fine, Vector y_fine, Vector2 f_fine) {
     cip::LinearInterp2D<double> interp2(x, y, f);
-    ASSERT_EQ(interp2.eval(0.5, 0.5), 2.0);
-    ASSERT_EQ(interp2.eval(1.5, 0.5), 2.75);
-    ASSERT_EQ(interp2.eval(0.5, 1.0), 2.5);
-    ASSERT_EQ(interp2.eval(1.0, 2.0), 3.0);
+    for ( auto i = 0; i < x_fine.size(); i++ ) {
+        for ( auto j = 0; j < y_fine.size(); j++ ) {
+            auto val = interp2.eval(x_fine[i], y_fine[j]);
+            if (!testing::internal::CmpHelperFloatingPointEQ<double>("expected", "actual", val, f_fine[i][j])  ) {
+                return testing::AssertionFailure()
+                    << "for x = " << x_fine[i] << ", y = " << y_fine[j] << "expected " << f_fine[i][j] << " but got " << val;
+            }
+        }
+    }
+    return testing::AssertionSuccess();
+}
 
 
+TEST(TestInterp2D, test_linear_interp_2d_normalized) {
+    Vector x = { 0.0, 1.0, 2.0 };
+    Vector y = { 0.0, 1.0, 2.0 };
+    Vector2 f = { 
+                { 1.0, 2.0, 2.0 }, 
+                { 2.0, 3.0, 3.0 }, 
+                { 3.0, 3.0, 4.0 }
+                };
+    Vector x_fine = { 0.0, 0.5, 1.0, 1.5, 2.0 };
+    Vector y_fine = { 0.0, 0.5, 1.0, 1.5, 2.0 };
+    Vector2 f_fine = { 
+                    { 1.0, 1.5, 2.0, 2.0, 2.0 }, 
+                    { 1.5, 2.0, 2.5, 2.5, 2.5 }, 
+                    { 2.0, 2.5, 3.0, 3.0, 3.0 }, 
+                    { 2.5, 2.75, 3.0, 3.25, 3.5 }, 
+                    { 3.0, 3.0, 3.0, 3.5, 4.0 }
+                    };
+    EXPECT_TRUE(Interp2DAssertions(x, y, f, x_fine, y_fine, f_fine));    
+}
+
+
+TEST(TestInterp2D, test_linear_interp_2d_standard) {
+    Vector x = { 0.0, 1.5, 3.0 };
+    Vector y = { 0.0, 2.0, 4.0 };
+    Vector2 f = { 
+                { 1.0, 2.0, 2.0 }, 
+                { 2.0, 3.0, 3.0 }, 
+                { 3.0, 3.0, 4.0 }
+                };
+    Vector x_fine = { 0.0, 0.75, 1.5, 2.25, 3.0 };
+    Vector y_fine = { 0.0, 1.0, 2.0, 3.0, 4.0 };
+    Vector2 f_fine = { 
+                    { 1.0, 1.5, 2.0, 2.0, 2.0 }, 
+                    { 1.5, 2.0, 2.5, 2.5, 2.5 }, 
+                    { 2.0, 2.5, 3.0, 3.0, 3.0 }, 
+                    { 2.5, 2.75, 3.0, 3.25, 3.5 }, 
+                    { 3.0, 3.0, 3.0, 3.5, 4.0 }
+                    };
+    EXPECT_TRUE(Interp2DAssertions(x, y, f, x_fine, y_fine, f_fine));    
+}
+
+
+TEST(TestInterp2D, test_linear_interp_2d_non_monotonic) {
+    Vector x = { 0.0, 1.0, 1.5 };
+    Vector y = { 0.0, 0.5, 3.0 };
+    Vector2 f = { 
+                { 1.0, 2.0, 2.0 }, 
+                { 2.0, 3.0, 3.0 }, 
+                { 3.0, 3.0, 4.0 }
+                };
+    Vector x_fine = { 0.0, 0.375, 0.75, 1.125, 1.5 };
+    Vector y_fine = { 0.0, 0.75, 1.5, 2.25, 3.0 };
+    Vector2 f_fine = { 
+                    { 1.0, 2.0, 2.0, 2.0, 2.0 }, 
+                    { 1.375, 2.375, 2.375, 2.375, 2.375 }, 
+                    { 1.75, 2.75, 2.75, 2.75, 2.75 }, 
+                    { 2.25, 3.025, 3.1, 3.175, 3.25 }, 
+                    { 3.0, 3.1, 3.4, 3.7, 4.0 }
+                    };
+    EXPECT_TRUE(Interp2DAssertions(x, y, f, x_fine, y_fine, f_fine));    
 }
---
+++ tests/utils/generate_test_data.py
@@ -0,0 +1,107 @@
+""" Generates test data for cubinterpp tests """
+
+import numpy as np
+from scipy.interpolate import RegularGridInterpolator
+
+
+def get_test_data(case='akima', start=1.0, end=5.0, size=8):
+    """ Generates test input data for Akima Spline tests """
+    if case == 'akima':
+        return np.array([1, 2, 3, 4.0, 5.0, 5.5, 7.0, 8.0, 9.0, 9.5, 10]), \
+               np.array([0, 0, 0, 0.5, 0.4, 1.2, 1.2, 0.1, 0.0, 0.3, 0.6])
+
+    if case == 'random':
+        return np.linspace(start, end, num=size), \
+               np.round(10.0 * np.random.rand(size))
+
+    return (start, end), (0, 1)
+
+
+def get_test_data_2d(case='standard'):
+    """ Generate 2D test data """
+    match case:
+        case 'normalized':
+            x = np.array([0.0, 1.0, 2.0])
+            y = np.array([0.0, 1.0, 2.0])
+            f = np.array([[1.0, 2.0, 2.0],
+                          [2.0, 3.0, 3.0],
+                          [3.0, 3.0, 4.0]])
+        case 'standard':
+            x = np.array([0.0, 1.5, 3.0])
+            y = np.array([0.0, 2.0, 4.0])
+            f = np.array([[1.0, 2.0, 2.0],
+                          [2.0, 3.0, 3.0],
+                          [3.0, 3.0, 4.0]])
+        case 'non-monotonic':
+            x = np.array([0.0, 1, 1.5])
+            y = np.array([0.0, 0.5, 3.0])
+            f = np.array([[1.0, 2.0, 2.0],
+                          [2.0, 3.0, 3.0],
+                          [3.0, 3.0, 4.0]])
+    return x, y, f
+
+
+def refine_grid(x_coord, size_fine=1000, extension=0):
+    """ Refines x grid and provide placeholder data for y """
+    return np.linspace(x_coord[0] - extension, x_coord[-1] + extension,
+                       num=size_fine)
+
+
+def scipy_linear_interp(x, y, f, x_fine, y_fine):
+    interp2 = RegularGridInterpolator((x, y), f)
+    x_grid, y_grid = np.meshgrid(x_fine, y_fine, indexing='ij')
+    return interp2((x_grid, y_grid))
+
+
+def cpp_array(array, indent=0):
+    """ Prints a nested array in C++ syntax """
+    yield (" " * indent) + "{ "
+    first = True
+    for value in array:
+        if isinstance(value, list) or isinstance(value, np.ndarray):
+            if not first:
+                yield ", "
+            yield "\n"
+            yield from cpp_array(value, indent=indent + 2)
+            printed_list = True
+        else:
+            if not first:
+                yield ", "
+            yield str(value)
+            printed_list = False
+        if first:
+            first = False
+    if printed_list:
+        yield "\n}"
+    else:
+        yield " }"
+
+
+def print_cpp_vector(vector_type, name, array):
+    """ Prints a nested array in C++ syntax """
+    array_str = "".join(cpp_array(array))
+    lhs = f'{vector_type} {name}'
+    indentation = len(lhs) + 3
+    base_str = '\n' + (' ' * indentation)
+    array_str = base_str.join(array_str.split('\n'))
+    rhs = f'{array_str};'
+    print(f'{lhs} = {rhs}')
+
+
+def main():
+
+    x, y, f = get_test_data_2d(case='non-monotonic')
+    x_fine = refine_grid(x, size_fine=5)
+    y_fine = refine_grid(y, size_fine=5)
+    f_fine = scipy_linear_interp(x, y, f, x_fine, y_fine)
+
+    print_cpp_vector('Vector', 'x', x)
+    print_cpp_vector('Vector', 'y', y)
+    print_cpp_vector('Vector2', 'f', f)
+    print_cpp_vector('Vector', 'x_fine', x_fine)
+    print_cpp_vector('Vector', 'y_fine', y_fine)
+    print_cpp_vector('Vector2', 'f_fine', f_fine)
+
+
+if __name__ == '__main__':
+    main()
