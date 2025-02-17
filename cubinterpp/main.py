1524a30e2adcbf84a11d8bcbe8ceaee75fc262c8
Sietze van Buuren <s.van.buuren@gmail.com>
2025-02-17 14:00:36 +0100

    test: Add better 2d data sets for compare 2D spline types
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- cubinterpp/main.py
+++ cubinterpp/main.py
@@ -42,7 +42,7 @@ def get_test_data_2d(case='standard'):
                         [3.0, 3.0, 4.0]])
         case 'akima':
             x = np.array([1, 2, 3, 4.0, 5.0, 5.5, 7.0, 8.0, 9.0, 9.5, 10])
-            y = x
+            y = np.array([1, 2, 3, 4.0, 5.0, 5.5, 7.0, 8.0, 9.0, 9.5, 10])
             f = np.array([[0, 0, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
                           [0, 0, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
                           [0, 0, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
@@ -55,6 +55,14 @@ def get_test_data_2d(case='standard'):
                           [0, 0, 0, 0.5, 0.4, 1.2, 1.2, 0.1, 0.0, 0.3, 0.3],
                           [0, 0, 0, 0.5, 0.4, 1.2, 1.2, 0.1, 0.0, 0.3, 0.6]])
             f *= 5 + 1
+        case 'three_bumps':
+            x = np.linspace(1, 10, 10)
+            y = np.linspace(1, 10, 10)
+            f = np.zeros((10, 10))
+            f[1:5, 1:5] = 0.45
+            f[5:7, 5:7] = np.array([[0.25, 0.2], [0.2, 0.25]])
+            f[7:9, 7:9] = 0.5
+            f = 10 * np.flip(f, axis=0) + 1
     return x, y, f
 
 
@@ -68,6 +76,26 @@ def scipy_linear_interp(x, y, f, x_fine, y_fine):
     interp2 = RegularGridInterpolator((x, y), f)
     x_grid, y_grid = np.meshgrid(x_fine, y_fine, indexing='ij')
     return interp2((x_grid, y_grid))
+
+
+def cubinterpp_interp2(interp_type='linear', data_case='akima', refinement=50):
+    """ Short hand for 2D interpolatino with cubinterpp """
+    x, y, f = get_test_data_2d(case=data_case)
+    match interp_type:
+        case 'linear':
+            interp2 = cubinterpp.LinearInterp2D(x, y, f)
+        case 'monotonic':
+            interp2 = cubinterpp.MonotonicSpline2D(x, y, f)
+        case 'akima':
+            interp2 = cubinterpp.AkimaSpline2D(x, y, f)
+        case 'natural_spline':
+            interp2 = cubinterpp.NaturalSpline2D(x, y, f)
+    x_fine, y_fine = refine_grid(x, refinement), refine_grid(y, refinement)
+    z_fine = np.zeros((len(x_fine), len(y_fine)))
+    for xi, xf in enumerate(x_fine):
+        for yi, yf in enumerate(y_fine):
+            z_fine[xi, yi] = interp2.eval(xf, yf)
+    return x_fine, y_fine, z_fine
 
 
 @mpg.plotter(projection='orthographic')
@@ -104,22 +132,20 @@ def main():
         'data points'
     )
 
-    x, y, f = get_test_data_2d(case='akima')
-    # interp2 = cubinterpp.LinearInterp2D(x, y, f)
-    interp2 = cubinterpp.MonotonicSpline2D(x, y, f)
-    # interp2 = cubinterpp.AkimaSpline2D(x, y, f)
-    # interp2 = cubinterpp.NaturalSpline2D(x, y, f)
-    x_fine, y_fine = refine_grid(x, 50), refine_grid(y, 50)
-    z_fine = np.zeros((len(x_fine), len(y_fine)))
-    for xi, xf in enumerate(x_fine):
-        for yi, yf in enumerate(y_fine):
-            z_fine[xi, yi] = interp2.eval(xf, yf)
+    for interp_type in ('linear', 'monotonic', 'akima', 'natural_spline'):
+        x_fine, y_fine, z_fine = cubinterpp_interp2(
+            interp_type=interp_type,
+            data_case='three_bumps',
+            refinement=46
+        )
+        title = interp_type.capitalize().replace('_', ' ')
+        mpg.figure(title=f'{title} interpolation', layout_type='Qt')
+        mpg.surf(x_fine, y_fine, z_fine)
+        ax = mpg.gca()
+        ax.azimuth = 225
 
-    mpg.figure(title='Test figure', layout_type='Qt')
-    mpg.surf(x_fine, y_fine, z_fine)
-    ax = mpg.gca()
-    ax.azimuth = 225
-
+    x, y, f = get_test_data_2d(case='three_bumps')
+    x_fine, y_fine = refine_grid(x, 46), refine_grid(y, 46)
     z_fine_scipy = scipy_linear_interp(x, y, f, x_fine, y_fine)
     mpg.figure(title='Test figure compare', layout_type='Qt')
     mpg.surf(x_fine, y_fine, z_fine_scipy)
