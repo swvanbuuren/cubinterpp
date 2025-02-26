6c35bd2d50e7df61a2a28abb1a7fb88ce8ca860f
Sietze van Buuren <s.van.buuren@gmail.com>
2025-02-26 11:23:35 +0100

    test: Modified tolerance for 1D and 2D assertions
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- tests/assertion_helpers.hpp
+++ tests/assertion_helpers.hpp
@@ -9,14 +9,14 @@ using Vector = std::vector<double>;
 using Vector2 = std::vector<Vector>;
 using Vector3 = std::vector<Vector2>;
 
-const double TOLERANCE = 5.0e-12;
+constexpr double TOLERANCE_DEFAULT = 5.0e-12;
 
-template <typename T>
+template <typename T, double tol=TOLERANCE_DEFAULT>
 testing::AssertionResult Interp1DAssertions(Vector x, Vector f, Vector x_fine, Vector f_fine) {
     T interp(x, f);
     for ( auto i = 0; i < x_fine.size(); i++ ) {
         auto val = interp.eval(x_fine[i]);
-        if (!testing::internal::DoubleNearPredFormat("expected", "actual", "tolerance", val, f_fine[i], TOLERANCE)  ) {
+        if (!testing::internal::DoubleNearPredFormat("expected", "actual", "tolerance", val, f_fine[i], tol)  ) {
             return testing::AssertionFailure()
                 << "for x = " << x_fine[i] << " expected " << f_fine[i] << " but got " << val;
         }
@@ -26,13 +26,13 @@ testing::AssertionResult Interp1DAssertions(Vector x, Vector f, Vector x_fine, V
 
 
 
-template <typename T>
+template <typename T, double tol=5.0e-7>
 testing::AssertionResult Interp2DAssertions(const Vector &x, const Vector &y, const Vector2 &f, const Vector &x_fine, const Vector &y_fine, const Vector2 &f_fine) {
     T interp2(x, y, f);
     for ( auto i = 0; i < x_fine.size(); ++i ) {
         for ( auto j = 0; j < y_fine.size(); ++j ) {
             auto val = interp2.eval(x_fine[i], y_fine[j]);
-            if (!testing::internal::DoubleNearPredFormat("expected", "actual", "tolerance", val, f_fine[i][j], TOLERANCE)  ) {
+            if (!testing::internal::DoubleNearPredFormat("expected", "actual", "tolerance", val, f_fine[i][j], tol)  ) {
                 return testing::AssertionFailure()
                     << "for x = " << x_fine[i] << ", y = " << y_fine[j] << " expected " << f_fine[i][j] << " but got " << val;
             }
@@ -42,14 +42,14 @@ testing::AssertionResult Interp2DAssertions(const Vector &x, const Vector &y, co
 }
 
 
-template <typename T>
+template <typename T, double tol=TOLERANCE_DEFAULT>
 testing::AssertionResult Interp3DAssertions(const Vector &x, const Vector &y, const Vector &z, const Vector3 &f, const Vector &x_fine, const Vector &y_fine, const Vector &z_fine, const Vector3 &f_fine) {
     T interp3(x, y, z, f);
     for ( auto i = 0; i < x_fine.size(); ++i ) {
         for ( auto j = 0; j < y_fine.size(); ++j ) {
             for ( auto k = 0; k < y_fine.size(); ++k ) {
                 auto val = interp3.eval(x_fine[i], y_fine[j], z_fine[k]);
-                if (!testing::internal::DoubleNearPredFormat("expected", "actual", "tolerance", val, f_fine[i][j][k], TOLERANCE)  ) {
+                if (!testing::internal::DoubleNearPredFormat("expected", "actual", "tolerance", val, f_fine[i][j][k], tol)  ) {
                     return testing::AssertionFailure()
                         << "for x = " << x_fine[i] << ", y = " << y_fine[j] << ", z = " << z_fine[j] << " expected " << f_fine[i][j][k] << " but got " << val;
                 }
