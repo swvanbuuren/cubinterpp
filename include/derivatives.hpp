0769a169b5d66939aade8af772bbebc7dff79ed6
Sietze van Buuren <s.van.buuren@gmail.com>
2025-03-28 20:23:32 +0100

    refactor: Enable to use imported lambda calc_slopes
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- include/derivatives.hpp
+++ include/derivatives.hpp
@@ -90,8 +90,8 @@ constexpr void for_each_dimension(const Tuple& tup, Func&& func) {
     for_each_dimension_impl<N>(tup, std::forward<Func>(func), std::make_index_sequence<N>{});
 }
 
-template <std::size_t N, typename FType, typename XiArray, typename DerivTuple>
-void compute_mixed_derivatives_impl(FType& F, XiArray const& xi, const DerivTuple& currentDeriv, std::size_t start = 0) {
+template <std::size_t N, typename FType, typename XiArray, typename DerivTuple, typename CalcSlopesFunctor>
+void compute_mixed_derivatives_impl(FType& F, XiArray const& xi, const DerivTuple& currentDeriv, CalcSlopesFunctor calcSlopes, std::size_t start = 0) {
     for_each_dimension<N>(currentDeriv, [&](auto d_const) {
         constexpr std::size_t D = d_const.value;
         if (D < start) {
@@ -102,19 +102,18 @@ void compute_mixed_derivatives_impl(FType& F, XiArray const& xi, const DerivTupl
             auto coord = build_coordinate_indices<N, D>(indicesTuple);
             auto extractionPattern = update_tuple_element<D>(currentDeriv, 0);
             auto f_slice = call_submdspan_1d(F, coord, extractionPattern);
-            auto slopes = calc_slopes(xi[D], f_slice);
             auto newDeriv = update_tuple_element<D>(currentDeriv, 1);
-            call_move_into_submdspan(F, slopes, coord, newDeriv);
+            call_move_into_submdspan(F, calcSlopes(xi[D], f_slice), coord, newDeriv);
         }, xi);
         auto newDeriv = update_tuple_element<D>(currentDeriv, 1);
-        compute_mixed_derivatives_impl<N>(F, xi, newDeriv, D+1);
+        compute_mixed_derivatives_impl<N>(F, xi, newDeriv, calcSlopes, D+1);
     });
 }
 
-template <std::size_t N, typename FType, typename XiArray>
-void compute_mixed_derivatives(FType& F, XiArray const& xi) {
+template <std::size_t N, typename FType, typename XiArray, typename CalcSlopesFunctor>
+void compute_mixed_derivatives(FType& F, XiArray const& xi, CalcSlopesFunctor calcSlopes) {
     auto basePattern = make_zero_tuple<N>();
-    compute_mixed_derivatives_impl<N>(F, xi, basePattern, 0);
+    compute_mixed_derivatives_impl<N>(F, xi, basePattern, calcSlopes, 0);
 }
 
 
